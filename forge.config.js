f9d3212f5b8e4ae7f7102f82a6737d487a7abec2
gukt <gukaitong@gmail.com>
2023-02-08 19:18:47 +0800

    Add App icon and dmg marker

--- forge.config.js
+++ forge.config.js
@@ -1,5 +1,38 @@
 module.exports = {
-  packagerConfig: {},
+  packagerConfig: {
+    // 这里全局提提供 icon 的设置（NOTE：不要提供后缀名）
+    // 这样就不需要在每个 maker 内部的 config 里配置 icon
+    icon: './assets/youdao-icon',
+    // 启用在 macOS 上进行代码签名。
+    // 在大多数情况下，osxSign 的配置附带了开箱即用的默认值，
+    // 因此我们建议您从一个空配置对象开始。
+    // https://www.electronforge.io/guides/code-signing/code-signing-macos
+    osxSign: {}, // object must exist even if empty
+    
+    // Option 1: Using an app-specific password
+    // https://www.electronforge.io/guides/code-signing/code-signing-macos#option-1-using-an-app-specific-password
+    // osxNotarize: {
+    //   tool: 'notarytool',
+    //   appleId: process.env.APPLE_ID,
+    //   appleIdPassword: process.env.APPLE_PASSWORD,
+    //   teamId: process.env.APPLE_TEAM_ID,
+    // }
+
+    // Option 2: Using an App Store Connect API key
+    // https://www.electronforge.io/guides/code-signing/code-signing-macos#option-2-using-an-app-store-connect-api-key
+    osxNotarize: {
+      tool: 'notarytool',
+      appleApiKey: process.env.APPLE_API_KEY,
+      appleApiKeyId: process.env.APPLE_API_KEY_ID,
+      appleApiIssuer: process.env.APPLE_API_ISSUER,
+    }
+
+    // Option 3: Using a keychain
+    // https://www.electronforge.io/guides/code-signing/code-signing-macos#option-3-using-a-keychain
+    // osxNotarize: {
+    //  ...
+    // }
+  },
   rebuildConfig: {},
   makers: [
     {
@@ -18,5 +51,17 @@ module.exports = {
       name: '@electron-forge/maker-rpm',
       config: {},
     },
+    {
+      name: '@electron-forge/maker-dmg',
+      // https://js.electronforge.io/interfaces/_electron_forge_maker_dmg.MakerDMGConfig.html
+      config: {
+        // 指定 DMG 图标
+        // https://www.electronforge.io/guides/create-and-add-icons
+        // icon: './assets/youdao-icon.icns',
+        // 指定 DMG 窗口的背景图片
+        background: './assets/dmg-bg.png',
+        format: 'ULFO'
+      }
+    }
   ],
 };
--- main.js
+++ main.js
@@ -1,20 +1,33 @@
+// 由于此文件是 Electron 的主进程脚本，所以它控制了应用程序主进程。
+// 它运行在一个完整的 Node.js 环境中，负责控制应用的生命周期，
+// 显示原生界面和执行底层的原生操作。
+// See also: https://www.electronjs.org/zh/docs/latest/tutorial/quick-start
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
+/**
+ * 创建一个新的浏览器窗口
+ */
 const createWindow = () => {
   // 创建一个浏览器窗口
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    // Web 页面功能设置
     webPreferences: {
+      // 指定一个脚本文件，该脚本文件在页面上所有文件运行之前加载。
+      // 此处的 preload 脚本可以获取到 Node.js 的 API
       preload: path.join(__dirname, 'preload.js')
     },
+    icon: path.join(__dirname, 'assets/youdao-icon.icns'),
   })
   // 在浏览器口中，加载工作目录下的 index.html
   win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
+  // 创建一个新的浏览器窗口
+  // 在 Electron 中，只有在 app 模块的 ready 事件被激活后才能创建浏览器窗口。
   createWindow()
 
   app.on('activate', () => {
@@ -33,3 +46,5 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
+// require('update-electron-app')()
