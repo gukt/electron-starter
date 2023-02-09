dff492d06c2c0f9ff63db26edf3e20469fae8776
gukt <gukaitong@gmail.com>
2023-02-09 20:08:50 +0800

    Remove sign and notarize

--- forge.config.js
+++ forge.config.js
@@ -7,7 +7,7 @@ module.exports = {
     // 在大多数情况下，osxSign 的配置附带了开箱即用的默认值，
     // 因此我们建议您从一个空配置对象开始。
     // https://www.electronforge.io/guides/code-signing/code-signing-macos
-    osxSign: {}, // object must exist even if empty
+    // osxSign: {}, // object must exist even if empty
     
     // Option 1: Using an app-specific password
     // https://www.electronforge.io/guides/code-signing/code-signing-macos#option-1-using-an-app-specific-password
@@ -20,12 +20,12 @@ module.exports = {
 
     // Option 2: Using an App Store Connect API key
     // https://www.electronforge.io/guides/code-signing/code-signing-macos#option-2-using-an-app-store-connect-api-key
-    osxNotarize: {
-      tool: 'notarytool',
-      appleApiKey: process.env.APPLE_API_KEY,
-      appleApiKeyId: process.env.APPLE_API_KEY_ID,
-      appleApiIssuer: process.env.APPLE_API_ISSUER,
-    }
+    // osxNotarize: {
+    //   tool: 'notarytool',
+    //   appleApiKey: process.env.APPLE_API_KEY,
+    //   appleApiKeyId: process.env.APPLE_API_KEY_ID,
+    //   appleApiIssuer: process.env.APPLE_API_ISSUER,
+    // }
 
     // Option 3: Using a keychain
     // https://www.electronforge.io/guides/code-signing/code-signing-macos#option-3-using-a-keychain
@@ -53,10 +53,9 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-dmg',
-      // https://js.electronforge.io/interfaces/_electron_forge_maker_dmg.MakerDMGConfig.html
+      // 更多关于 config 的配置项，请参考：https://js.electronforge.io/interfaces/_electron_forge_maker_dmg.MakerDMGConfig.html
       config: {
-        // 指定 DMG 图标
-        // https://www.electronforge.io/guides/create-and-add-icons
+        // 指定 DMG 图标：https://www.electronforge.io/guides/create-and-add-icons
         // icon: './assets/youdao-icon.icns',
         // 指定 DMG 窗口的背景图片
         background: './assets/dmg-bg.png',
